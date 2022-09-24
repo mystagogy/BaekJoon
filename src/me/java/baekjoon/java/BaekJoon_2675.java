f31e28bbbcd07b0b32ac7a6f00a233021f2bcc91
mystagogy <hi7747@naver.com>
2022-09-24 21:03:07 +0900

    feat: Solution of #2675 problem from Baek_Joon.

---
+++ src/me/java/baekjoon/java/BaekJoon_2675.java
@@ -0,0 +1,21 @@
+package me.java.baekjoon.java;
+
+import java.util.Scanner;
+
+public class BaekJoon_2675 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int test_num = sc.nextInt();
+            for(int i = 0; i<test_num; i++){
+                int num = sc.nextInt();
+                String str = sc.next();
+                String result = "";
+                for(int j=0; j<str.length(); j++){
+                    for(int k = 0; k < num; k++)
+                        result += str.charAt(j);
+                }
+                System.out.println(result);
+            }
+        }
+}
+
