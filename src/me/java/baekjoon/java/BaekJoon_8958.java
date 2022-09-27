2102ddf6d1acc00f2ac40325995f58ff7e358a51
mystagogy <hi7747@naver.com>
2022-09-27 15:36:37 +0900

    The solution of #8958 of BaekJoon.
    This calculate score of quiz.

---
+++ src/me/java/baekjoon/java/BaekJoon_8958.java
@@ -0,0 +1,26 @@
+package me.java.baekjoon.java;
+import java.util.Scanner;
+public class BaekJoon_8958{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int test_num = sc.nextInt();
+        int point = 0;
+        int total = 0;
+        for(int i =0; i<test_num; i++){
+            String[] result = new String[test_num];
+            result[i] = sc.next();
+            char[] ox = result[i].toCharArray();
+            point = 0;
+            total = 0;
+            for(int j = 0; j< ox.length; j++){
+                if('X' == ox[j]){
+                    point = 0;
+                }else{
+                    point++;
+                    total += point;
+                }
+            }
+            System.out.println(total);
+        }
+    }
+}
