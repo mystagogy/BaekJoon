870cf0b6593be51638a6d635a28d33875b99b2d5
mystagogy <hi7747@naver.com>
2022-10-01 20:29:54 +0900

    The solution of secret_map of Programmers

---
+++ src/me/java/programmers/SecretMap.java
@@ -0,0 +1,18 @@
+package me.java.programmers;
+/*
+프로그래머스 코딩테스트 연습 레벨1
+2018 KAKAO BLIND RECRUITMENT - 비밀지도
+ */
+public class SecretMap {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for (int i=0; i<n; i++) {
+            String tmp = Integer.toBinaryString(arr1[i] | arr2[i]);
+            tmp = String.format("%" + n + "s", tmp);
+            tmp = tmp.replaceAll("1", "#");
+            tmp = tmp.replaceAll("0", " ");
+            answer[i] = tmp;
+        }
+        return answer;
+    }
+}
