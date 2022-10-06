88e899c05a812b9be3858e0ecfda7fd9025f42a6
mystagogy <hi7747@naver.com>
2022-10-06 23:31:00 +0900

    The solution of #10814 of BaekJoon

---
+++ src/me/java/baekjoon/java/BeakJoon_10814.java
@@ -0,0 +1,54 @@
+package me.java.baekjoon.java;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+class Person {
+
+    private int age;
+    private String name;
+
+    public Person() {};
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class BeakJoon_10814 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            Person[] person = new Person[num];
+
+            for(int i = 0; i < num; i++){
+                person[i] = new Person(sc.nextInt(), sc.next());
+            }
+            Arrays.sort(person, new Comparator<Person>() {
+                @Override
+                public int compare(Person o1, Person o2) {
+                    return o1.getAge() - o2.getAge();
+                }
+            });
+            for(int i = 0; i < num; i++)
+                System.out.println(person[i]);
+        }
+    }
+
