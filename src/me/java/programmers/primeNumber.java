12ac58f02d865945ed2491106519d4170a3739d6
mystagogy <hi7747@naver.com>
2022-10-25 20:30:45 +0900

    The solution of counting prime number using Sieve of Eratosthenes

---
+++ src/me/java/programmers/primeNumber.java
@@ -0,0 +1,30 @@
+package me.java.programmers;
+
+public class primeNumber {
+    public static int solution(int n) {
+        int answer = 0;
+        int[] arr =new int[n+1];
+        for(int i = 2 ; i <= n; i++){
+            arr[i] = i;
+        }
+        for(int i =2; i*i <= n; i++){
+            if(arr[i]==0)
+                continue;
+            for(int j = i*i; j <= n; j+=i){
+                arr[j] = 0;
+            }
+        }
+        for(int i = 0; i <= n; i++){
+            if(arr[i] != 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(2));
+        System.out.println(solution(10));
+        System.out.println(solution(5));
+    }
+}
