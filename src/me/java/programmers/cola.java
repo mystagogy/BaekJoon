95d783b553400afbdd1993bfdf0d1c9547fb9b8b
mystagogy <hi7747@naver.com>
2022-11-21 15:11:43 +0900

    The solution of cola from Programmers

---
+++ src/me/java/programmers/cola.java
@@ -0,0 +1,25 @@
+package me.java.programmers;
+
+public class cola {
+    public static int solution(int a, int b, int n) {
+        int answer = 0;
+        int rest = 0;
+        int get = 0;
+        boolean state = true;
+        while(state){
+            rest = n%a;
+            get = (n/a)*b;
+            n = get+rest;
+            if(n < a){
+                state = false;
+            }
+            answer += get;
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3,1,20));
+    }
+}
