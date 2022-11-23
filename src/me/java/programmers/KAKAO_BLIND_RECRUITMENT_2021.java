ba42f57b44f6ecff819b92e3904432aed676dfca
mystagogy <hi7747@naver.com>
2022-11-23 17:27:15 +0900

    The solution of  2021 KAKAO BLIND RECRUITMENT from Programmers

---
+++ src/me/java/programmers/KAKAO_BLIND_RECRUITMENT_2021.java
@@ -0,0 +1,37 @@
+package me.java.programmers;
+
+public class KAKAO_BLIND_RECRUITMENT_2021 {
+    public String solution(String new_id) {
+        String answer = "";
+
+        new_id = new_id.toLowerCase(); //1단계
+
+        new_id = new_id.replaceAll("[^\\w\\-_.]*",""); //2단계
+
+        new_id = new_id.replaceAll("\\.{2,}",".");   //3단계
+
+        new_id = new_id.replaceAll("^[.]|[.]$","");  //4단계
+
+        if (new_id.length()==0) {   //5단계
+            new_id = "a";
+        }
+
+        if(new_id.length()>15){ //6단계
+            new_id = new_id.substring(0,15);
+            new_id = new_id.replaceAll("[.]$","");
+        }
+
+        if(new_id.length() < 3){    //7단계
+            boolean state = true;
+            while(state){
+                new_id += new_id.substring(new_id.length()-1);
+                if(new_id.length() > 2) state = false;
+            }
+        }
+
+        answer = new_id;
+
+        return answer;
+    }
+
+}
