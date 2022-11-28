171f39cc0e33604525df8183e67556a863aad68d
mystagogy <hi7747@naver.com>
2022-11-28 19:24:04 +0900

    The solution of #1927 from BaekJoon

---
+++ src/me/java/baekjoon/BeakJoon_1927.java
@@ -0,0 +1,33 @@
+package me.java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BeakJoon_1927 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        StringBuffer sb = new StringBuffer();
+        try{
+            int num = Integer.parseInt(br.readLine());
+            for(int i = 0; i < num; i++){
+                int x = Integer.parseInt(br.readLine());
+                if(x==0){
+                    if(queue.isEmpty()){
+                        sb.append(0).append("\n");
+                    }
+                    else{
+                        sb.append(queue.poll()).append("\n");
+                    }
+                }
+                else
+                    queue.offer(x);
+            }
+            System.out.println(sb);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
