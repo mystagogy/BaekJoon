c8fa86ce0d3c147ee1ab96535f535fe575d136b9
mystagogy <hi7747@naver.com>
2022-11-29 13:13:47 +0900

    The solution of Babbling from BaekJoon

---
+++ src/me/java/programmers/Babbling.java
@@ -0,0 +1,30 @@
+package me.java.programmers;
+
+public class Babbling {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+
+        for(int i=0; i<babbling.length; i++){
+
+            if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") ||
+                    babbling[i].contains("mama") || babbling[i].contains("woowoo"))
+                continue;
+
+            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace(" ", "");
+
+            if(babbling[i].length()==0){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"aya", "yee", "u", "maa"}));
+    }
+}
