aa4c7827831bd342f43534f2d94095c4107f7bb3
mystagogy <hi7747@naver.com>
2022-11-30 16:26:20 +0900

    The solution of #10814 from BaekJoon

---
+++ src/me/java/programmers/HOF.java
@@ -0,0 +1,31 @@
+package me.java.programmers;
+
+import java.util.*;
+
+public class HOF {
+    public static int[] solution(int k, int[] score) {
+        int[] answer = {};
+        List<Integer>list = new ArrayList<>();
+        answer = new int[score.length];
+        int i = 0;
+        for(int num : score){
+            if(list.size() < k){
+                list.add(num);
+            }else{
+                if(list.get(0) < num){
+                    list.remove(0);
+                    list.add(num);
+                }
+            }
+            Collections.sort(list);
+            answer[i] = list.get(0);
+            i++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, new int[]{10, 100, 20, 150, 1, 100, 200}));
+        System.out.println(solution(4, new int[]{0, 300, 40, 300, 20, 70, 150, 50, 500, 1000}));
+    }
+}
