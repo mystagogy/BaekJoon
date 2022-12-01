573abded460522c555866c7fb5c04f521af472f0
mystagogy <hi7747@naver.com>
2022-12-01 15:29:55 +0900

    The solution of weapon from Programmers

---
+++ src/me/java/programmers/weapon.java
@@ -0,0 +1,29 @@
+package me.java.programmers;
+
+public class weapon {
+    public static int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] count = new int[number];
+        for(int i = 0; i < count.length; i++){
+            int x = i+1;
+            int cnt = 0;
+            for(int j = 1; j*j <= x; j++){
+                if(j*j == x) cnt++;
+                else if(x%j==0) cnt+=2;
+                count[i] = cnt;
+            }
+            count[i] = cnt;
+        }
+
+        for(int i = 0 ; i < count.length; i++){
+            if(count[i] > limit){
+                count[i] = power;
+            }
+        }
+
+        for(int result : count){
+            answer+=result;
+        }
+        return answer;
+    }
+}
