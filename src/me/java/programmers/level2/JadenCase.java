9d1f3ba3e7db773a0ca8bf67666a497fccc71a07
mystagogy <hi7747@naver.com>
2022-12-08 14:51:42 +0900

    The solution of programmers

---
+++ src/me/java/programmers/level2/JadenCase.java
@@ -0,0 +1,48 @@
+package me.java.programmers.level2;
+
+import java.util.StringTokenizer;
+
+public class JadenCase {
+    public static String solution(String s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        String[] strings = s.split(" ");
+        for(String str : strings){
+            boolean state = true;
+            while (state){
+                if(str.length()==0){
+                    sb.append(str);
+                    state = false;
+                }
+                else if(str.charAt(0) >= 'A' && str.charAt(0) <= 'Z'){
+                    sb.append(str.charAt(0)).append(str.substring(1).toLowerCase());
+                    state = false;
+                }
+                else if(str.charAt(0) >= 'a' && str.charAt(0) <= 'z'){
+                    sb.append(Character.toUpperCase(str.charAt(0))).append(str.substring(1).toLowerCase());
+                    state = false;
+                }
+                else {
+                    sb.append(str.charAt(0)).append(str.substring(1).toLowerCase());
+                    state = false;
+                }
+            }
+            sb.append(" ");
+        }
+
+        if(s.substring(s.length()-1).equals(" ")){
+            answer = String.valueOf(sb);
+        }
+        else{
+            answer = sb.substring(0,sb.length()-1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("3people unFollowed me"));
+        System.out.println(solution("for the last week"));
+        System.out.println(solution("A Aa "));
+        System.out.println(solution("a  aa "));
+    }
+}
