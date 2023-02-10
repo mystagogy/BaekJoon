3092096c8a76a15550fed7d4291ee36fb0e8aa25
mystagogy <hi7747@naver.com>
2023-02-10 21:47:06 +0900

    The solution of #2747 BaekJoon

---
+++ src/me/java/baekjoon/BaekJoon_2747.java
@@ -0,0 +1,18 @@
+package me.java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon_2747 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] fibo = new int[n+1];
+        fibo[1] = 1;
+        for(int i = 2; i <= n; i++){
+            fibo[i] = fibo[i-1] + fibo[i-2];
+        }
+        System.out.println(fibo[n]);
+    }
+}
