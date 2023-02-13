a05aca7ad8f5011bf8974765df4017933f94cb33
mystagogy <hi7747@naver.com>
2023-02-13 19:53:48 +0900

    The solution of #1904 BaekJoon

---
+++ src/me/java/baekjoon/BaekJoon_1904.java
@@ -0,0 +1,19 @@
+package me.java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon_1904 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%15746;
+        }
+        System.out.println(dp[n]);
+    }
+}
