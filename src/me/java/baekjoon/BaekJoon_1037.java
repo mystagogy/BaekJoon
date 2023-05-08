918eea27e5d7cc0d9fe7cc6efcaf601aeb6b4d88
mystagogy <hi7747@naver.com>
2023-05-08 16:17:44 +0900

    The solution of #1037

---
+++ src/me/java/baekjoon/BaekJoon_1037.java
@@ -0,0 +1,31 @@
+package me.java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon_1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if(N==1){
+            System.out.println(arr[0]*2);
+            return;
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] * arr[N-1]);
+    }
+}
