9c1c08f1875221173e25032051162479d10478d3
mystagogy <hi7747@naver.com>
2023-05-12 17:14:44 +0900

    The solution of #15651

---
+++ src/me/java/baekjoon/BaekJoon_15651.java
@@ -0,0 +1,42 @@
+package me.java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_15651 {
+
+    static int N, M;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    static void dfs(int depth){
+        if(M==depth){
+            for(int x : arr){
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            visited[i] = true;
+            arr[depth] = i+1;
+            dfs(depth+1);
+            visited[i] = false;
+
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        arr = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
+}
