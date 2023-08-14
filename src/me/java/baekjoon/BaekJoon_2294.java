5af8ca4e69ebb25af28269426f735445a06d8076
mystagogy <hi7747@naver.com>
2023-08-14 17:38:46 +0900

    The solution of #2294

---
+++ src/me/java/baekjoon/BaekJoon_2294.java
@@ -0,0 +1,39 @@
+package me.java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_2294 {
+    static int n,k;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        k=Integer.parseInt(st.nextToken());
+        arr=new int[n+1];
+
+        for(int i=1;i<=n;i++) {
+            st=new StringTokenizer(br.readLine());
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp=new int[k+1];
+
+        for(int i=1;i<=k;i++){
+            dp[i]=Integer.MAX_VALUE-1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=arr[i];j<=k;j++){
+                dp[j]=Math.min(dp[j],dp[j-arr[i]]+1);
+            }
+        }
+
+        if(dp[k]==Integer.MAX_VALUE-1) System.out.print(-1);
+        else System.out.print(dp[k]);
+
+    }
+}
