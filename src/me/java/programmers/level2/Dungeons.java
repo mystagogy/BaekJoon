55216791ea7d5ed6f2154aa747e0cc5df454a79f
mystagogy <hi7747@naver.com>
2023-09-26 14:50:49 +0900

    The solution of programmers

--- src/me/java/programmers/level2/Dungeons.java
+++ src/me/java/programmers/level2/Dungeons.java
@@ -1,9 +1,26 @@
 package me.java.programmers.level2;
 
 public class Dungeons {
-    public int solution(int k, int[][] dungeons) {
-        int answer = -1;
+    static int max = 0;
+    static boolean[] visited;
+    public void dfs(int[][] dungeons, boolean[] visited, int k, int count){
+        for(int i = 0; i < dungeons.length; i++){
+            if(!visited[i]  && k >= dungeons[i][0]){
+                visited[i] = true;
+                dfs(dungeons, visited, k - dungeons[i][1], count + 1);
+                visited[i] = false;
+            }
+        }
 
-        return answer;
+        if(max < count){
+            max = count;
+        }
+    }
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(dungeons, visited, k, 0);
+
+        return max;
+
     }
 }
