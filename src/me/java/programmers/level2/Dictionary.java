1791f4ce57d69a47230e5371ca16f4744caf3c8f
mystagogy <hi7747@naver.com>
2023-10-06 17:15:56 +0900

    The solution of Programmers

---
+++ src/me/java/programmers/level2/Dictionary.java
@@ -0,0 +1,15 @@
+package me.java.programmers.level2;
+
+public class Dictionary {
+    static final String MAPPING = "AEIOU";
+    static final int[] RATE_OF_INCREASE = {781, 156, 31, 6, 1};
+    public int solution(String word) {
+        int answer = word.length();
+
+        for (int i = 0; i < word.length(); i++) {
+            answer += (RATE_OF_INCREASE[i] * MAPPING.indexOf(word.charAt(i)));
+        }
+
+        return answer;
+    }
+}
