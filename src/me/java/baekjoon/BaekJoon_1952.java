fbbb5c9b1c4ea74a00180c8e5363799f986621b2
mystagogy <hi7747@naver.com>
2023-11-02 15:06:46 +0900

    The solution of #1952

---
+++ src/me/java/baekjoon/BaekJoon_1952.java
@@ -0,0 +1,22 @@
+package me.java.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_1952 {
+    static int M,N;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        if(M == N || M < N) System.out.println(2*(M-1));
+        else System.out.println(2*(N-1)+1);
+
+
+    }
+}
